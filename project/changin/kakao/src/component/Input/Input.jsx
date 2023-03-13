f43efd41535ffd32203e09a73d3c8679011d071c
백창인 <baek-changin@baegchang-in-ui-MacBookPro.local>
2023-03-13 18:24:24 +0900

    kakao

---
+++ project/changin/kakao/src/component/Input/Input.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Input.css";
+
+function Input() {
+    
+    return (
+        <div className="input-cont">
+            <p>+</p>
+            <input type="text" name="message" />
+        </div>
+    )
+}
+
+export { Input };
