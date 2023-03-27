4505e982103349867f16ddd4d0e94d7f48488cef
99bci <99bci@naver.com>
2023-03-27 18:01:21 +0900

    chapter 2 revised

--- example/chapter2/kakao_example/src/component/Input/Input.jsx
+++ example/chapter2/kakao_example/src/component/Input/Input.jsx
@@ -1,21 +1,29 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./Input.css";
 
+function Input({ data, setter }) {
+  const [inputChat, setInputChat] = useState("");
 
-function Input({data, setter}) {
-const [inputChat, setInputChat] = useState("")
-
-    return (
-        <div className="input-cont">
-            <button onClick={()=>{
-                setter([...data, inputChat])
-                setInputChat("")
-            }}>+</button>
-            <input type="text" name="message" value={inputChat} onChange={(e)=>{
-              setInputChat(e.target.value);
-            }}/>
-        </div>
-    )
+  return (
+    <div className="input-cont">
+      <button
+        onClick={() => {
+          setter([...data, inputChat]);
+          setInputChat("");
+        }}
+      >
+        +
+      </button>
+      <input
+        type="text"
+        name="message"
+        value={inputChat}
+        onChange={(e) => {
+          setInputChat(e.target.value);
+        }}
+      />
+    </div>
+  );
 }
 
 export { Input };
